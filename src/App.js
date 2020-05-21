ee32e8001ae9850c83fbe00c881f5ce649c3a420
Sunil Kashyap <sunil@innocirc.com>
2020-05-21 14:56:58 +0530

    added route

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
-          <Route exact path="/" component={Home}/>
+          <Route exact path="/e-com-react" component={Home}/>
           <Route exact path="/cart" component={Cart}/>
         </Switch>
       </Router>
