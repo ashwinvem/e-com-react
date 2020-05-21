1b62c2e1e1dbc7ae79cd8b617f064ad118727e59
Sunil Kashyap <sunil@innocirc.com>
2020-05-21 12:20:50 +0530

    Updates

---
+++ precache-manifest.ce083bf31861d06e120d3b5547021073.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a17fe38fbd6dd9bf7d98983d09d24274",
+    "url": "/e-com-react/index.html"
+  },
+  {
+    "revision": "8090500d0dccab6e35ae",
+    "url": "/e-com-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d18e8a09e1790d8ed6c7",
+    "url": "/e-com-react/static/css/main.41e2edb1.chunk.css"
+  },
+  {
+    "revision": "8090500d0dccab6e35ae",
+    "url": "/e-com-react/static/js/2.2fbd5df3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/e-com-react/static/js/2.2fbd5df3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d18e8a09e1790d8ed6c7",
+    "url": "/e-com-react/static/js/main.cc112966.chunk.js"
+  },
+  {
+    "revision": "2d9501798e1a98ea74d7",
+    "url": "/e-com-react/static/js/runtime-main.3959737a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/e-com-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
